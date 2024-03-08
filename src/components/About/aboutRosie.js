03015ccb7b1ee3004b780dfaa2897c56f7285089
elijahsamuels <elijahsamuels@gmail.com>
2024-03-08 08:48:19 -0700

    slight refactor of About

---
+++ src/components/About/aboutRosie.js
@@ -0,0 +1,36 @@
+export const aboutRosie = {
+	intro: {
+		title: null,
+    paragraph:
+		"Welcome to Rosie's corner of the internet! Rosie is not just any ordinary pup; she's a charismatic canine with a heart of gold and a bundle of energy that never seems to wane. Let us introduce you to the wonderful world of Rosie.",
+  },
+	name: {
+		title: "Name",
+		paragraph:
+			"Rosie",
+	},
+  breed: {
+    title: "Breed",
+    paragraph: "Australian Cattle Dog, Husky, Catahoula Leopard Dog mix",
+  },
+  coat: {
+    title: "Coat",
+    paragraph:
+      "Rosie boasts a sleek black coat adorned with striking brindle white boots and a chest that catches the eye of all who meet her. With her short hair, she's both stylish and low-maintenance, the perfect combination for an active lifestyle.",
+  },
+  tricks: {
+    title: "Tricks",
+    paragraph:
+      "Rosie is not your average dog when it comes to intelligence. She has mastered over twenty tricks, showcasing her sharp mind and eagerness to learn. Whether it's playing fetch with her ChuckIt wheel, rolling over, or even sleeping in the sun, Rosie is always ready to impress with her extensive repertoire of tricks.",
+  },
+  personality: {
+    title: "Personality",
+    paragraph:
+      "What truly sets Rosie apart is her vibrant personality. She radiates love and affection, making friends wherever she goes. Her playful nature knows no bounds, and she's always up for a game of fetch or a romp in the park. But don't be fooled by her energetic demeanor; Rosie also has a gentle and nurturing side, making her the perfect cuddle buddy after a long day.",
+  },
+  mission: {
+    title: "Mission",
+    paragraph:
+      "Rosie's mission is simple: to spread joy and positivity wherever she roams. Whether she's brightening up your Instagram feed with her adorable antics or bringing a smile to your face during a visit to the local dog park, Rosie is here to remind us all of the simple pleasures in life.",
+  },
+}
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,58 +1,31 @@
 import React from "react";
 import "./styles.css";
+import {aboutRosie} from  "./aboutRosie";
 
-const introParagraph = `Welcome to Rosie's corner of the internet! Rosie is not just any ordinary pup; she's a charismatic canine with a heart of gold and a bundle of energy that never seems to wane. Let us introduce you to the wonderful world of Rosie.`;
+const AboutSubject = () => {
+  const sections = [];
+  for (const entry in aboutRosie) {
+    const section = aboutRosie[entry];
+    const { title, paragraph } = section;
 
-const closingParagraph = `So come along and join Rosie on her adventures! Follow her journey as she navigates the world one wag of her tail at a time, and prepare to be captivated by the charm and charisma of this extraordinary pup.`;
+    sections.push(
+      <section className="section" key={title}>
+				{title && (
+					<p className={"subtitle"}>{title}</p>
+				)}
+        <p className={"paragraph"}>{paragraph}</p>
+      </section>
+    );
+  }
 
-// const aboutRosie = `
+  return sections;
+};
 
 const About = () => {
   return (
     <div className="container">
       <div className="title">About Rosie</div>
-      <p>{introParagraph}</p>
-      <section className="something">
-        <p className={"subtitle"}>Name: </p>
-        <p className={"paragraph"}>Rosie</p>
-      </section>
-
-      <section className="something">
-        <p className={"subtitle"}>Breed: </p>
-        <p className={"paragraph"}>
-          Australian Cattle Dog, Husky, Catahoula Leopard Dog mix
-        </p>
-      </section>
-
-      <section className="something">
-        <p className={"subtitle"}>Coat: </p>
-        <p className={"paragraph"}>
-				Rosie boasts a sleek black coat adorned with striking brindle white boots and a chest that catches the eye of all who meet her. With her short hair, she's both stylish and low-maintenance, the perfect combination for an active lifestyle.
-        </p>
-      </section>
-
-      <section className="something">
-        <p className={"subtitle"}>Tricks: </p>
-        <p className={"paragraph"}>
-				Rosie is not your average dog when it comes to intelligence. She has mastered over twenty tricks, showcasing her sharp mind and eagerness to learn. Whether it's playing fetch with her ChuckIt wheel, rolling over, or even sleeping in the sun, Rosie is always ready to impress with her extensive repertoire of tricks.
-        </p>
-      </section>
-
-      <section className="something">
-        <p className={"subtitle"}>Personality: </p>
-        <p className={"paragraph"}>
-				What truly sets Rosie apart is her vibrant personality. She radiates love and affection, making friends wherever she goes. Her playful nature knows no bounds, and she's always up for a game of fetch or a romp in the park. But don't be fooled by her energetic demeanor; Rosie also has a gentle and nurturing side, making her the perfect cuddle buddy after a long day.
-        </p>
-      </section>
-
-      <section className="something">
-        <p className={"subtitle"}>Mission: </p>
-        <p className={"paragraph"}>
-				Rosie's mission is simple: to spread joy and positivity wherever she roams. Whether she's brightening up your Instagram feed with her adorable antics or bringing a smile to your face during a visit to the local dog park, Rosie is here to remind us all of the simple pleasures in life.`;
-        </p>
-      </section>
-
-      <p>{closingParagraph}</p>
+      <AboutSubject />
     </div>
   );
 };
