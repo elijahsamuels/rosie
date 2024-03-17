c5a6f026c1f83313932c18d7becfe2798103eb84
elijahsamuels <elijahsamuels@gmail.com>
2024-03-17 17:29:36 -0600

    updated welcome component. still working on the title, but it is very close.

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Rosie from '../../images/Rosie2.png'
-import './styles.css'
+import React from "react";
+import Rosie from "../../images/Rosie2.png";
+import "./styles.css";
+
+const str = "ROSIE THE BAT DOG";
+
+const Title = () => {
+  let newTitle = [];
+  for (let i = 0; i < str.length; i++) {
+    newTitle.push(
+      <div class="wrapper">
+        <div id={str[i]} class="letter">
+          {str[i]}
+        </div>
+        <div class="shadow">{str[i]}</div>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div class="overlay"></div>
+      <div class="text">{newTitle}</div>
+    </>
+  );
+};
 
 const Welcome = () => {
-	return (
-		<div className='welcome-container'>Welcome
-			<img src={Rosie} alt="Rosie" className='welcomePhoto' />
-		</div>
-	)
-}
+  return (
+    <div className="welcome-container">
+      <div className="title">
+        <Title />
+      </div>
+      {/* <img src={Rosie} alt="Rosie" className="welcomePhoto" />
+      <img src={Rosie} alt="Rosie" className="welcomePhoto" />
+      <img src={Rosie} alt="Rosie" className="welcomePhoto" />
+      <img src={Rosie} alt="Rosie" className="welcomePhoto" />
+      <img src={Rosie} alt="Rosie" className="welcomePhoto" />
+      <img src={Rosie} alt="Rosie" className="welcomePhoto" /> */}
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
