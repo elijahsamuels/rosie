8fd2d200304d275473d0fd2fe498d25be758a174
Elijah Samuels <elijahsamuels@gmail.com>
2025-12-25 18:26:59 -0700

    updating graphql request

--- netlify/functions/gitlab-proxy.js
+++ netlify/functions/gitlab-proxy.js
@@ -1,73 +1,74 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 
-import axios from "axios";
-// const { GITLAB_API_TOKEN: ACCESS_TOKEN } = process.env;
-
-// https://gitlab.com/-/graphql-explorer
 const pipelinesQuery = `
 query {
-	project(fullPath: "elijahsamuels/rosie") {
-		pipelines(first: 25) {
-			nodes {
-				id
-				status
-				failureReason
-				createdAt
-				updatedAt
-				stages {
-					nodes {
-						name
-						status
-						jobs {
-							nodes {
-								name
-							}
-						}
-					}
-				}
-				jobArtifacts { 
-					downloadPath
-					fileType
-					name
-					size
-					expireAt
-				}
-			}
-		}
-	}
+  project(fullPath: "elijahsamuels/rosie") {
+    pipelines(first: 25) {
+      nodes {
+        id
+        status
+        failureReason
+        createdAt
+        updatedAt
+        stages {
+          nodes {
+            name
+            status
+            jobs {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+        jobArtifacts { 
+          downloadPath
+          fileType
+          name
+          size
+          expireAt
+        }
+      }
+    }
+  }
 }`;
 
-export const handler = async () => {
-  // const response = await fetch('https://gitlab.com/api/v4/...', {
-  //   headers: { 'Authorization': `Bearer ${process.env.GITLAB_API_TOKEN}` }
-  // });
-  // const data = await response.json();
-  // return { statusCode: 200, body: JSON.stringify(data) };
-
-  let data = JSON.stringify({
-    query: pipelinesQuery,
-    variables: {},
-  });
-
-  let config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "https://gitlab.com/api/graphql",
-    headers: {
-      "Content-Type": "application/json",
-      // "Private-Token": ACCESS_TOKEN,
-    },
-    data,
-  };
-
-  let axiosResponse = axios
-    .request(config)
-    .then((response) => {
-      const { data } = response.data;
-      return data;
-    })
-    .catch((error) => {
-      console.log(error);
+export const handler = async (event) => {
+  try {
+    const response = await fetch("https://gitlab.com/api/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Private-Token": process.env.GITLAB_API_TOKEN,
+      },
+      body: JSON.stringify({
+        query: pipelinesQuery,
+        variables: {},
+      }),
     });
-  return axiosResponse;
+
+    const result = await response.json();
+
+    if (result.errors) {
+      console.error("GitLab GraphQL Errors:", result.errors);
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ errors: result.errors }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(result.data),
+    };
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error", details: error.message }),
+    };
+  }
 };
