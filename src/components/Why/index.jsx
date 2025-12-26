d5252438b130b06e11eadbad22983759d0528c7f
Elijah Samuels <elijahsamuels@gmail.com>
2025-12-25 19:01:06 -0700

    updating graphql request

--- src/components/Why/index.jsx
+++ src/components/Why/index.jsx
@@ -1,31 +1,21 @@
 import React, { useState, useEffect } from "react";
-// import { fetchPipelineData, fetchJobData } from "../../utils/getGitlabData";
-import { getGitlabPipelineDataGraphQL} from "../../utils/getGitlabPipelineDataGraphQL";
+import { getGitlabPipelineDataGraphQL } from "../../utils/getGitlabPipelineDataGraphQL";
 import { paragraphs } from "./constants";
 import { iconMap } from "./iconMap";
-import Loading from '../Loading';
+import Loading from "../Loading";
 import "./styles.css";
 
 const Why = () => {
-  // const [jobData, setJobData] = useState([]);
   const [pipelineData, setPipelineData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // const getJobData = async () => {
-    //   let data = await fetchJobData();
-    //   setJobData(data);
-    // };
-    // const getPipelineData = async () => {
-    //   let data = await fetchPipelineData();
-    //   setPipelineData(data);
-    // };
     const getPipelineData = async () => {
       let data = await getGitlabPipelineDataGraphQL();
-			if (data.project?.pipelines?.nodes){
-				setPipelineData(data.project?.pipelines?.nodes);
-			}
-      setIsLoading(false); 
+      if (data?.project?.pipelines?.nodes) {
+        setPipelineData(data?.project?.pipelines?.nodes);
+      }
+      setIsLoading(false);
     };
 
     getPipelineData();
@@ -41,22 +31,16 @@ const Why = () => {
     });
   };
 
-	const parseDate = (dateString) => {
+  const parseDate = (dateString) => {
+    return dateString.replace("T", " ").replace("Z", " ");
+  };
 
-		return dateString.replace('T', ' ').replace('Z', ' ') // "2024-04-05T03:32:02Z"
-		
-		// const year = new Date(dateString).getFullYear()
-		// const month = new Date(dateString).getMonth()
-		// const day = new Date(dateString).getDate()
-		// const hours = new Date(dateString).getHours()
-		// const minutes = new Date(dateString).getMinutes()
-		
-		// return `${year} / ${month} / ${day} @ ${hours}:${minutes}`
-	}
-
-	const iconGenerator = (name, index = 0) => {
-		// console.log('index:', index);
-		return <span key={index} className={`${iconMap[name].class} icon`}>{iconMap[name].shortName} </span>;
+  const iconGenerator = (name, index = 0) => {
+    return (
+      <span key={index} className={`${iconMap[name].class} icon`}>
+        {iconMap[name].shortName}{" "}
+      </span>
+    );
   };
 
   const JobDataTable = () => {
@@ -93,17 +77,18 @@ const Why = () => {
             <td className="status">{node.status}</td>
             <td className="stage">{node.stages.nodes[0]?.name || null}</td>
             {/* <td className="job-from-stage">{node.stages.nodes.map(node => node.jobs.nodes.map((subNode, index) => subNode.name )) || null}</td> */}
-            <td className="job-from-stage">{node.stages.nodes.map(node => node.jobs.nodes.map((subNode, index) => iconGenerator(subNode.name, index) )) || null}</td>
+            <td className="job-from-stage">
+              {node.stages.nodes.map((node) =>
+                node.jobs.nodes.map((subNode, index) => iconGenerator(subNode.name, index))
+              ) || null}
+            </td>
             {/* <td className="stage">{node.stages.nodes[0]?.stages || null}</td>.map(job => job.name */}
             <td className="failureReason">{node.failureReason || "N/A"}</td>
             <td className="datetime">{parseDate(node.createdAt) || "N/A"}</td>
             <td className="link">
-              <a
-                href={`https://gitlab.com${node.jobArtifacts[0]?.downloadPath}`}>
+              <a href={`https://gitlab.com${node.jobArtifacts[0]?.downloadPath}`}>
                 {node.jobArtifacts[0]
-                  ? `${node.jobArtifacts[0].name} @ ${parseInt(
-                      node.jobArtifacts[0].size / 1000
-                    )}kb`
+                  ? `${node.jobArtifacts[0].name} @ ${parseInt(node.jobArtifacts[0].size / 1000)}kb`
                   : ""}
               </a>
             </td>
@@ -113,37 +98,37 @@ const Why = () => {
     });
   };
 
-const Legend = () => {
-  return (
-    <div className="icon-legend">
-      <b>Icon Legend</b>
+  const Legend = () => {
+    return (
+      <div className="icon-legend">
+        <b>Icon Legend</b>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Short Name</th>
-            <th>Long Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
+        <table>
+          <thead>
+            <tr>
+              <th>Short Name</th>
+              <th>Long Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {Object.entries(iconMap).map((item, index) => {
-						const parentProperties = Object.keys(iconMap);
+          <tbody>
+            {Object.entries(iconMap).map((item, index) => {
+              const parentProperties = Object.keys(iconMap);
 
-            return (
-              <tr key={index}>
-								{iconGenerator(parentProperties[index], index) || null}
-                <td className="long-name">{item[1].longName}</td>
-                <td className="description">{item[1].description}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+              return (
+                <tr key={index}>
+                  {iconGenerator(parentProperties[index], index) || null}
+                  <td className="long-name">{item[1].longName}</td>
+                  <td className="description">{item[1].description}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
   return (
     <div className="container">
@@ -152,7 +137,7 @@ const Legend = () => {
         <WhyParagraphs />
       </div>
       {(isLoading && <Loading />) || <JobDataTable />}
-			<Legend />
+      <Legend />
     </div>
   );
 };
--- src/utils/getGitlabPipelineDataGraphQL.js
+++ src/utils/getGitlabPipelineDataGraphQL.js
@@ -1,61 +1,11 @@
 import axios from "axios";
-import { pipelinesQuery } from "./graphQLQueries";
 
 export const getGitlabPipelineDataGraphQL = async () => {
-  const data = JSON.stringify({
-    query: pipelinesQuery,
-    variables: {},
-  });
-
   try {
-    // This URL works in PROD and LOCALLY (via Netlify Dev)
-    const response = await axios.post("/.netlify/functions/gitlab-proxy", data);
-		console.log('response.data', response.data.data);
-    return response.data.data; 
+    const response = await axios.post("/.netlify/functions/gitlab-proxy");
+    return response.data;
   } catch (error) {
     console.error("Error fetching GitLab data:", error);
     throw error;
   }
 };
-
-// import axios from "axios";
-// import { pipelinesQuery } from "./graphQLQueries";
-// import { handler } from "../../netlify/functions/gitlab-proxy.js";
-
-// // https://docs.gitlab.com/ee/api/graphql/reference/#pipeline
-// export const getGitlabPipelineDataGraphQL = async () => {
-//   let data = JSON.stringify({
-//     query: pipelinesQuery,
-//     variables: {},
-//   });
-
-//   if (process.env.NODE_ENV === "local") {
-//     let config = {
-//       method: "post",
-//       maxBodyLength: Infinity,
-//       url: "https://gitlab.com/api/graphql",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Private-Token": ACCESS_TOKEN,
-//       },
-//       data,
-//     };
-
-//     let axiosResponse = axios
-//       .request(config)
-//       .then((response) => {
-//         const { data } = response.data;
-//         return data;
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//     return axiosResponse;
-//   } else {
-//     // /.netlify/functions/gitlab-proxy
-
-//     const data = await handler.then((response) => response.data).catch((error) => error);
-
-//     return data;
-//   }
-// };
