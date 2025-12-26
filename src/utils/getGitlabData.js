366a1fd3c63d162ef6cd452f72894586519634e9
Elijah Samuels <elijahsamuels@gmail.com>
2025-12-25 17:49:27 -0700

    updating graphql request

--- src/utils/getGitlabData.js
+++ src/utils/getGitlabData.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-const {
-  REACT_APP_GITLAB_API_TOKEN: ACCESS_TOKEN,
-  REACT_APP_GITLAB_PROJECT_ID: PROJECT_ID,
-  REACT_APP_GITLAB_BASE_URL: BASE_URL,
-} = process.env;
+// const {
+//   REACT_APP_GITLAB_API_TOKEN: ACCESS_TOKEN,
+//   REACT_APP_GITLAB_PROJECT_ID: PROJECT_ID,
+//   REACT_APP_GITLAB_BASE_URL: BASE_URL,
+// } = process.env;
 
 const config = {
   method: "get",
   maxBodyLength: Infinity,
   headers: {
-    "PRIVATE-TOKEN": ACCESS_TOKEN,
+    // "PRIVATE-TOKEN": ACCESS_TOKEN,
   },
 };
 
@@ -24,14 +24,14 @@ const fetchRepositoryData = async (config) => {
 };
 
 export const fetchPipelineData = async () => {
-  config.url = `${BASE_URL}${PROJECT_ID}/pipelines?per_page=11`;
+  // config.url = `${BASE_URL}${PROJECT_ID}/pipelines?per_page=11`;
   const data = await fetchRepositoryData(config);
 
   return data;
 };
 
 export const fetchJobData = async () => {
-  config.url = `${BASE_URL}${PROJECT_ID}/jobs?per_page=10`;
+  // config.url = `${BASE_URL}${PROJECT_ID}/jobs?per_page=10`;
   const data = await fetchRepositoryData(config);
 
   return data;
